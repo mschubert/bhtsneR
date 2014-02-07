b43e6ac0fd7b50dc7371f7560b3160b58f6cf3d1
mschubert <mschu.dev@gmail.com>
2014-02-07 10:25:29 +0000

    add rcpp skeleton

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,11 +1,12 @@
 #include <Rcpp.h>
-#include "src/tsne.h"
+#include "tsne.h"
+using namespace Rcpp;
 
 RcppExport SEXP bh_sne(SEXP X, int no_dims=2, double perplexity=30, double theta=0.5) {
 BEGIN_RCPP
 
     // create matrix from SEXP
-    Rcpp::NumericMatrix X_r(X);
+    NumericMatrix X_r(X);
     int N = X_r.nrow();
     int D = X_r.ncol();
 
@@ -17,10 +18,10 @@ BEGIN_RCPP
 
     // run TSNE
     TSNE tsne = TSNE();
-    tsne.run(&X_c, N, D, &Y_c, no_dims, perplexity, theta);
+    tsne.run(X_c, N, D, &Y_c, no_dims, perplexity, theta);
 
     // return an R matrix
-    return Rcpp::Matrix::create(Y_c)
+    return Matrix::create(Y_c)
 
 END_RCPP
 }
